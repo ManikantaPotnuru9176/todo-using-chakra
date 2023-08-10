29f5d68469d627b4f1eb82bc47d0af648ccc3e1e
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-08-10 18:15:20 +0530

    chore: changes to vercel errors

--- pages/components/TodoList.js
+++ pages/components/TodoList.js
@@ -12,38 +12,39 @@ import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 const TodoList = ({ todos, handleComplete, handleEdit, handleDelete }) => {
   return (
     <VStack spacing="4" w="40vw" h="60vh">
-      {todos.map((todo) => (
-        <Card key={todo.id} padding="20px" w="400px">
-          <HStack>
-            <Text
-              w="250px"
-              as={todo.complete ? "s" : "p"}
-              onClick={() => handleComplete(todo.id)}
-            >
-              {todo.task}
-            </Text>
-            <Spacer />
-            <IconButton
-              isRound={true}
-              variant="solid"
-              colorScheme="teal"
-              aria-label="Edit"
-              fontSize="20px"
-              icon={<EditIcon />}
-              onClick={() => handleEdit(todo)}
-            />
-            <IconButton
-              isRound={true}
-              variant="solid"
-              colorScheme="red"
-              aria-label="Delete"
-              fontSize="20px"
-              icon={<DeleteIcon />}
-              onClick={() => handleDelete(todo.id)}
-            />
-          </HStack>
-        </Card>
-      ))}
+      {todos &&
+        todos.map((todo) => (
+          <Card key={todo.id} padding="20px" w="400px">
+            <HStack>
+              <Text
+                w="250px"
+                as={todo.complete ? "s" : "p"}
+                onClick={() => handleComplete(todo.id)}
+              >
+                {todo.task}
+              </Text>
+              <Spacer />
+              <IconButton
+                isRound={true}
+                variant="solid"
+                colorScheme="teal"
+                aria-label="Edit"
+                fontSize="20px"
+                icon={<EditIcon />}
+                onClick={() => handleEdit(todo)}
+              />
+              <IconButton
+                isRound={true}
+                variant="solid"
+                colorScheme="red"
+                aria-label="Delete"
+                fontSize="20px"
+                icon={<DeleteIcon />}
+                onClick={() => handleDelete(todo.id)}
+              />
+            </HStack>
+          </Card>
+        ))}
     </VStack>
   );
 };
